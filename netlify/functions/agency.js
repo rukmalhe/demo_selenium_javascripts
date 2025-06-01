fa6b19a0ca8b7078bb5132527f8ac2df18b9921a
Rasika Rukmal <rukmal.hewawasam@gmail.com>
2025-06-01 19:33:45 +1200

    added DB connection

--- netlify/functions/agency.js
+++ netlify/functions/agency.js
@@ -1,9 +1,32 @@
+const { MongoClient } = require("mongodb");
+
 const handler = async () => {
+  const client = new MongoClient(process.env.CONNECTIONSTRING); // Created an environment variable in Netlify Project
+
+  await client.connect();
+
+  const agencyClient = await client.db().collection("clients").find().toArray();
+  client.close;
+
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: "Hi"
+    headers: { "Content-Type": "application/json", "access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(agencyClient)
   };
 };
 
 module.exports = { handler };
+
+/*
+
+//http://localhost:8888/.netlify/functions/agency
+
+async function vacancies(params) {
+  const vacancies = await fetch("")
+  const vacancyData = await vacancies.json()
+  vacancyData.forEach(agencyClient => { //anonymous function, can be called by parameter itself, without mentioning the function name or parenthisis
+    console.log(agencyClient.name)
+  })
+}
+
+*/
